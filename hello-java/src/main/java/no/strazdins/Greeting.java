21e784982d1fd602eb01d8f76289d4c3f37a1fdc
Girts Strazdins <girts.strazdins@gmail.com>
2024-04-19 16:33:37 +0200

    CheckStyle should be OK now

--- hello-java/src/main/java/no/strazdins/Greeting.java
+++ hello-java/src/main/java/no/strazdins/Greeting.java
@@ -1,9 +1,21 @@
 package no.strazdins;
 
+/**
+ * A class for creating greetings.
+ */
 public class Greeting {
+  /**
+   * Not allowed to instantiate the object.
+   */
   private Greeting() {
   }
 
+  /**
+   * Create a Greeting.
+   *
+   * @param name name of the person/object to greet, use "World" by default
+   * @return Hello ${name}!
+   */
   public static String createGreeting(String name) {
     if (name == null || name.isEmpty()) {
       name = "World";
