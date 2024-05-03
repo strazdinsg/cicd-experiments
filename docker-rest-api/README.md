efc5b260c86aa18f88a576364bcbe36c55a3a8bd
Girts Strazdins <girts.strazdins@gmail.com>
2024-05-03 17:53:22 +0200

    Clone the rest-api to a separate dir, separate port

---
+++ docker-rest-api/README.md
@@ -0,0 +1,3 @@
+# Simple REST API
+
+Proof of concept REST API for custom deployment on Ubuntu server.
---
+++ docker-rest-api/src/main/java/no/strazdins/hello/GreetingController.java
@@ -0,0 +1,22 @@
+package no.strazdins.hello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * A REST API controller which responds to HTTP requests.
+ */
+@RestController
+public class GreetingController {
+
+    /**
+     * This method is called when HTTP GET / is received
+     *
+     * @return The message to be sent in the HTTP response's body
+     */
+    @GetMapping("/")
+    public String greeting() {
+        return "Ieraudzīsi, uzzināsi!";
+    }
+}
+
